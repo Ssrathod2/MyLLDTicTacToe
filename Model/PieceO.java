7b614aabb430381c6a3ae8e05a6133e76a5efda8
Sandeep Rathod <rathodssm2001@gmail.com>
2024-10-30 22:25:44 +0530

    Created Model For MyTicTacToe Game

---
+++ Model/PieceO.java
@@ -0,0 +1,7 @@
+package Gitprojects.MyLLDTicTacToe.Model;
+
+public class PieceO extends Piece {
+    public PieceO(){
+        super(PieceType.O);
+    }
+}
---
+++ Model/Player.java
@@ -0,0 +1,29 @@
+package Gitprojects.MyLLDTicTacToe.Model;
+
+public class Player {
+
+    public String name;
+    public Piece piece;
+
+    public Player(String name, Piece piece){
+        this.name = name;
+        this.piece= piece;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public Piece getPiece(){
+        return piece;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPiece(Piece piece){
+        this.piece = piece;
+    }
+    
+}
